c422a5e0cfa9a2e8b66e07a95711e1841129c124
Brandon Chin <bbrandonchin@yahoo.com>
2022-11-29 00:33:31 +0000

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,47 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log(addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log(convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+var getGreetingresult = getGreeting('World');
+console.log(getGreetingresult);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+console.log(addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+var multiplyAndDivideBy55Result = multiplyAndDivideBy5(35, 10);
+console.log(multiplyAndDivideBy55Result);
+
+function getCircleCircumference(radius) {
+  return (2 * 3.14159265359 * radius);
+}
+var getCircleCircumferenceResult = getCircleCircumference(5);
+console.log(getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var getFullNameResult = getFullName('Juan', 'Ramirez');
+console.log(getFullNameResult);
+
+function cube(number) {
+  return (number * number * number);
+}
+var cubeResult = cube(5);
+console.log(cubeResult);
